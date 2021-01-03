9cf526147a47223e3469126818b9bae2aee4707b
irfa ardiansyah <irfardy2@gmail.com>
2021-01-03 21:01:57 +0700

    Array

--- hello.rs
+++ hello.rs
@@ -1,13 +1,29 @@
-fn main() {
+fn main() 
+{
+
 	let x = 5;
 	let y = 23;
     let count = hitung(x,y);
+
     println!("Hasil dari {} + {} adalah {}", x, y, count);
 }
+
 fn hitung(x: i32,y:i32) -> i32
 {
 	let count = x+y;
 
 	return count;
 }
+
+fn name()
+{
+	return "Irfa Ardiansyah";
+}
+
+fn nyoba_array()
+{
+	let arr=["Harta","Tahta","Himeka"]
+	return arr
+}
+
 //Irfa Ardiansyah
